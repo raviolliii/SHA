fdaa7cc48fabcca213c8595350e4be86e896adca
Ravi Patel <ravi.patel1245@gmail.com>
2019-02-07 10:54:43 -0500

    Added SHA256 algorithm

--- test.js
+++ test.js
@@ -5,21 +5,38 @@
 //================================================================
 
 const SHA1 = require("./SHA1.js");
+const SHA256 = require("./SHA256.js");
 
 function pass(value) {
     console.log("\n\x1b[32m\x1b[1m%s\x1b[0m", value);
+}
+
+function warn(value) {
+    console.log("\n\x1b[33m\x1b[1m%s\x1b[0m", value);
 }
 
 function fail(value) {
     console.log("\n\x1b[31m\x1b[1m%s\x1b[0m", value);
 }
 
-let message = "Yeah I like coffee, how'd you know?";
+let message = "tester";
 
-let hh = SHA1(message);
-let ans = "44792ad8239c9fd53bca3b929df625f834ae8341";
+let hh = SHA256(message);
+let ans = "9bba5c53a0545e0c80184b946153c9f58387e3bd1d4ee35740f29ac2e718b019";
 
-console.log("\nMessage:   " + message);
-console.log("Hash:      " + hh);
-console.log("Answer:    " + ans);
-hh === ans ? pass("[ Passed ✔  ]") : fail("[ Failed ✘ ]");
+if (ans) {
+	if (hh !== ans) {
+	    console.log("\nMessage:   " + message);
+	    console.log("Hash:      " + hh);
+	    console.log("Answer:    " + ans);
+	    fail("[ Failed ✘ ]");
+	    return;
+	}
+    console.log("\nMessage:   " + message);
+    console.log("Hash:      " + hh);
+    console.log("Answer:    " + ans);
+	pass("[ Passed ✔  ]");
+	return;
+}
+
+warn("Hash: " + hh);
